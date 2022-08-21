54f4178f2d0c07fad1284d4e99cc1d0203dbd148
William <William.Jeffery.Knight@protonmail.com>
2022-08-21 15:01:09 -0400

    create first httpresponse

--- main/views.py
+++ main/views.py
@@ -1,3 +1,7 @@
+import re
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(response):
+    return HttpResponse('<h1>Hello World!!</h1>')
