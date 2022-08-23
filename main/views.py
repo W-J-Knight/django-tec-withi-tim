4ce80fccef03c02939b76e695ac1c458c4a14e8e
William <William.Jeffery.Knight@protonmail.com>
2022-08-23 10:04:04 -0400

    http://127.0.0.1:8000/ type any number after display on web page

--- main/views.py
+++ main/views.py
@@ -1,10 +1,10 @@
 import re
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 # Create your views here.
 
-def index(response):
-    return HttpResponse('<h1>Hello World!!</h1>')
+def index(response, id):
+    # ls = ToDoList.objects.get(id=id)
+    return HttpResponse('<h1>%s</h1>' % id)
 
-def v1(response):
-    return HttpResponse('<h1>view 1 !!!</h1>')
